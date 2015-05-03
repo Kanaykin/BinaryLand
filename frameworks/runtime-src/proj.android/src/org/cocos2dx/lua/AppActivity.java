be4f01abb0146b67fbc7310ef72ac662127f2961
Sergey Kanaykin <kanaykin.sergey@gmail.com>
2015-05-03 23:27:20 +0300

    refs #??? hide bottom bar

--- frameworks/runtime-src/proj.android/src/org/cocos2dx/lua/AppActivity.java
+++ frameworks/runtime-src/proj.android/src/org/cocos2dx/lua/AppActivity.java
@@ -31,6 +31,8 @@ import android.util.Log;
 import org.cocos2dx.lib.Cocos2dxActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.os.Build.VERSION_CODES;
 
 import org.myextend.MyExtendHelper;
 import com.google.android.gms.common.ConnectionResult;
@@ -58,6 +60,7 @@ GoogleApiClient.OnConnectionFailedListener
     
 	private GoogleApiClient 		mPlusClient;
 	private String					mDocumentDirectory;
+	private int 					mCurrentApiVersion;
 		
     public static boolean isSdPresent() {
     	return android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);
@@ -69,12 +72,7 @@ GoogleApiClient.OnConnectionFailedListener
     
     private native void nativeSetPaths(String documents_path);
     
-	@Override
-	protected void onCreate(final Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		Log.e("SQC", "onCreate");
-		
+    protected void initDirectories() {
 		String strCacheDirectory = getExternalCacheDir().getAbsolutePath();
 		
 		ApplicationInfo applicationInfo = getApplicationInfo();
@@ -89,7 +87,7 @@ GoogleApiClient.OnConnectionFailedListener
         
         nativeSetPaths(mDocumentDirectory);
         
-        File fileName = new File(mDocumentDirectory, "temp2.txt");
+        /*File fileName = new File(mDocumentDirectory, "temp2.txt");
         if (!fileName.exists())
         	//fileName.mkdirs();
         	try {
@@ -97,8 +95,69 @@ GoogleApiClient.OnConnectionFailedListener
         	f.write("hello world");
         	f.flush();
         	f.close();
-        	} catch (Exception e) {}
+        	} catch (Exception e) {}*/
+
+    }
+    
+    protected void initUIImmersive() {
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        
+        mCurrentApiVersion = android.os.Build.VERSION.SDK_INT;
+        
+        // This work only for android 4.4+
+        if(mCurrentApiVersion >= android.os.Build.VERSION_CODES.KITKAT)
+        {
+            getWindow().getDecorView().setSystemUiVisibility(flags);
+            
+            // Code below is to handle presses of Volume up or Volume down.
+            // Without this, after pressing volume buttons, the navigation bar will
+            // show up and won't hide
+            final View decorView = getWindow().getDecorView();
+            decorView
+                .setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
+                {
+
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility)
+                    {
+                        if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
+                        {
+                            decorView.setSystemUiVisibility(flags);
+                        }
+                    }
+                });
+        }
+    }
+    
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        if(mCurrentApiVersion >= android.os.Build.VERSION_CODES.KITKAT) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                  View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+    
+	@Override
+	protected void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		
+		Log.e("SQC", "onCreate");
+
+		initDirectories();
+		
+		initUIImmersive();
 		//mPlusClient =  new GoogleApiClient.Builder(this)
 	    //	.addApi(Plus.API)
 	    //	.addScope(Plus.SCOPE_PLUS_LOGIN)
