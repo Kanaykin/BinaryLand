2e50b20a9f7c8c3c4b2c6876ea58c8c92c9cbf29
Sergey Kanaykin <kanaykin.sergey@gmail.com>
2015-12-16 09:47:27 +0300

    refs #58 minor fix

--- src/levels/Level1_3_bonus.lua
+++ src/levels/Level1_3_bonus.lua
@@ -1,6 +1,6 @@
 local CustomProperties = {
     ["7_1_109"] = {state = PlayerObject.PLAYER_STATE.PS_OBJECT_IN_TRAP},
-    ["15_4_113"] = {chestBonusType = BonusObject.CHEST_TIME_TYPE}
+    ["15_4_113"] = {ChestType = BonusObject.CHEST_TIME_TYPE}
 }
 
 return CustomProperties;
