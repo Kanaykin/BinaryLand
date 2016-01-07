4f562f78d26c4ba5e2eb94e1d8f838ed27aea113
Sergey Kanaykin <kanaykin.sergey@gmail.com>
2016-01-07 21:45:29 +0300

    refs #61 loading screen

--- src/scenes/LoadingScene.lua
+++ src/scenes/LoadingScene.lua
@@ -1,13 +1,20 @@
 require "src/scenes/BaseScene"
 require "src/base/Log"
+require "src/gui/GuiHelper"
 
 local LOADSCEENIMAGE = "Loading.png"
 local TIME_SHOWING = 3.0
+
 --[[
 loading scene - loading screen
 --]]
 LoadingScene = inheritsFrom(BaseScene)
 LoadingScene.mTimer = TIME_SHOWING;
+LoadingScene.mProgress = nil;
+LoadingScene.mTimeLabel = nil;
+LoadingScene.PROGRESS_TAG = 31;
+LoadingScene.LABEL_TAG = 32;
+LoadingScene.PROGRESS_PARENT_TAG = 30;
 
 ---------------------------------
 function LoadingScene:destroy()
@@ -15,12 +22,32 @@ function LoadingScene:destroy()
 end
 
 --------------------------------
+function LoadingScene:loadScene()
+    local ccpproxy = CCBProxy:create();
+    local reader = ccpproxy:createCCBReader();
+    local node = ccpproxy:readCCBFromFile("LoadingScene", reader, false);
+    self.mSceneGame:addChild(node);
+
+    local parent = node:getChildByTag(LoadingScene.PROGRESS_PARENT_TAG);
+    local parentSize = CCDirector:getInstance():getVisibleSize();
+    parent:setPositionX(parentSize.width / 2);
+    self.mProgress = tolua.cast(parent:getChildByTag(LoadingScene.PROGRESS_TAG), "ccui.Scale9Sprite");
+    self.mProgress:setScaleX(0);
+
+    self.mTimeLabel = node:getChildByTag(LoadingScene.LABEL_TAG);
+    self.mTimeLabel:setPositionX(parentSize.width / 2);
+    setDefaultFont(self.mTimeLabel, self.mSceneManager.mGame:getScale());
+end
+
+--------------------------------
 function LoadingScene:init(sceneMan, params)
-	self:superClass().init(self, sceneMan, {background = LOADSCEENIMAGE});
+	self:superClass().init(self, sceneMan, {});
 	info_log("LoadingScene:init");
 
     local statistic = extend.Statistic:getInstance();
     statistic:sendEvent("setScreenName", "LoadingScene");
+
+    self:loadScene();
 end
 
 ---------------------------------
@@ -30,4 +57,9 @@ function LoadingScene:tick(dt)
     if self.mTimer <= 0 then
         self.mSceneManager:runNextScene();
     end
+    local percent = ( TIME_SHOWING - self.mTimer) / TIME_SHOWING;
+    self.mProgress:setScaleX(percent);
+
+    local textLabel = tostring(math.floor(percent * 100 / 5) * 5);
+    self.mTimeLabel:setString(textLabel.."%");
 end
