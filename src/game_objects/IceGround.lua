7f9037e36f11772da3b54e2619645b331253aa1d
Sergey Kanaykin <kanaykin.sergey@gmail.com>
2016-05-13 22:37:38 +0300

    refs #71 fix ice ground

--- src/game_objects/IceGround.lua
+++ src/game_objects/IceGround.lua
@@ -2,14 +2,25 @@ require "src/game_objects/Trigger"
 
 IceGround = inheritsFrom(Trigger)
 
+function round(n)
+  return math.floor((math.floor(n*2) + 1)/2)
+end
+
 ---------------------------------
 function IceGround:onEnter(player)
+	-- info_log("IceGround:onEnter 0.94 ", round(0.94));
+	-- info_log("IceGround:onEnter -0.94 ", round(-0.94));
+	-- info_log("IceGround:onEnter 0.12 ", round(0.12));
+	-- info_log("IceGround:onEnter -0.12 ", round(-0.12));
 	info_log("IceGround:onEnter ");
 	IceGround:superClass().onEnter(self, player);
+	self:updateOrder();
 	--local delta = player.mDelta;
 	info_log("IceGround:onEnter delta ", player:getLastButtonPressed());
 	if player.mDelta or (player:getLastButtonPressed() and DIRECTIONS[player:getLastButtonPressed()]) then
 		local newDir = player.mDelta and player.mDelta:normalized() or DIRECTIONS[player:getLastButtonPressed()]:clone() * player.mReverse;
+		newDir.x = round(newDir.x);
+		newDir.y = round(newDir.y);
 		info_log("IceGround:onEnter delta.x ", newDir.x, " delta.y ", newDir.y);
 
 		local gridPos = Vector.new(self.mField:getGridPosition(self.mNode));
@@ -22,8 +33,19 @@ function IceGround:onEnter(player)
 		--player:playAnimation(nil);
 		--player:moveTo(gridPos);
 		player:enterTrap(self.mField:gridPosToReal(gridPos));
-		player.mMoveFinishCallback = nil
+		player.mMoveFinishCallback = nil;
+		player.mInTrap = false;
 	end
+end
+
+--------------------------------
+function IceGround:updateOrder()
+	debug_log("IceGround:updateOrder ")
+	local gridPosition = Vector.new(self.mField:getGridPosition(self.mNode));
+	local prevOrderPos = -gridPosition.y * 5;
+	local parent = self.mNode:getParent();
+	parent:removeChild(self.mNode, false);
+	parent:addChild(self.mNode, prevOrderPos);
 end
 
 ---------------------------------
--- src/levels/1.lua
+++ src/levels/1.lua
@@ -1,20 +1,11 @@
-local map = {0,0,8,0,0,0,0,0,0,0,0,0,14,0,0,0,0,15,12,0,0,0,0,12,13,0,0,0,0,0,0,0,5,6,0,0}
+local map = {0,0,8,0,0,0,0,0,0,0,0,0,0,0,17,0,0,0,0,0,17,0,0,0,0,0,17,0,0,0,0,0,5,6,0,0}
 local level = {
-time = 120
+time = 120,
+BonusLevel = ''
 }
 local CustomProperties = {
-["1_4_112"] = {
-BonusFile= require "src/levels/bonus"},
-["6_4_112"] = {
-BonusFile=''},
-["1_3_113"] = {
-ChestType=1,
-Count=40},
-["6_3_113"] = {
-ChestType=0,
-Count=20},
 ["3_1_109"] = {
-InTrap=true},
+InTrap=false},
 ["4_1_108"] = {
 InTrap=false}}
 return{ m = map,w=6,h=6,levelParams=level,CustomProperties=CustomProperties}
