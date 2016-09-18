fe8b043b0f765c9bc8b0dd5046ed4fc298a06092
Sergey Kanaykin <kanaykin.sergey@gmail.com>
2016-09-18 22:03:19 +0300

    refs #80 1-6 level

---
+++ src/levels/Level4_6.lua
@@ -0,0 +1,10 @@
+local CustomProperties = {
+    ["11_1_116"] = {DestPoint = "11,5"},
+    ["9_4_116"] = {DestPoint = "13,4"},
+    ["9_2_116"] = {DestPoint = "13,2"},
+    ["13_8_116"] = {DestPoint = "9,8"},
+    ["13_7_116"] = {DestPoint = "10,7"},
+    ["13_6_116"] = {DestPoint = "9,6"}
+}
+
+return CustomProperties;
